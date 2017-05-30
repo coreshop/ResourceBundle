d293fc0fac9626248b4b229971186fe8d0e653a6
dpfaffenbauer <dominik@lineofcode.at>
2017-05-30 16:20:50 +0200

    [ResourceBundle, All] add possiblity to define type of Pimcore Model and Install File. Add Command to install all Pimcore Models (Object, Brick, Fieldcollection). Refactor CoreBundle to use resource command for file installation

---
+++ Command/InstallResourcesCommand.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\ResourceBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class InstallResourcesCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('coreshop:resources:install')
+            ->setDescription('Install Resource registered Pimcore Classes.')
+            ->setHelp(<<<EOT
+The <info>%command.name%</info> command creates Pimcore Classes.
+EOT
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $outputStyle = new SymfonyStyle($input, $output);
+        $outputStyle->writeln(sprintf(
+            'Creating Pimcore classes <info>%s</info>.',
+            $this->getContainer()->get('kernel')->getEnvironment()
+        ));
+
+        $this
+            ->getContainer()->get('coreshop.resource.installer.classes')
+            ->installResources($output);
+
+        return 0;
+    }
+}
