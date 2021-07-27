f3e2c0270bf1a0696c5bef998b64f39c327d2205
paulverdu <paulverdu20@gmail.com>
2021-07-27 10:32:35 +0200

    Prevent setting $id to null when inserting
    This will prevent $id being set to null and throw an error when inserting a new object and resource id is set to null. Return type is int|string and so $id can not be null.

--- Pimcore/ObjectManager.php
+++ Pimcore/ObjectManager.php
@@ -167,7 +167,7 @@ final class ObjectManager implements \Doctrine\Persistence\ObjectManager
         $id = spl_object_hash($resource);
 
         if (method_exists($resource, 'getId')) {
-            $id = $resource->getId();
+            $id = $resource->getId() ?? $id;
         }
 
         return $id;
