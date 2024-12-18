9116a4f8a56135d3b98faa7074180f2b059faa42
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-12-18 16:39:57 +0100

    [ResourceBundle] Version Marshalling for FieldCollections

---
+++ DeepCopy/PimcoreFieldCollectionDefinitionMatcher.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * CoreShop
+ *
+ * This source file is available under two different licenses:
+ *  - GNU General Public License version 3 (GPLv3)
+ *  - CoreShop Commercial License (CCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.org)
+ * @license    https://www.coreshop.org/license     GPLv3 and CCL
+ *
+ */
+
+namespace CoreShop\Bundle\ResourceBundle\DeepCopy;
+
+use DeepCopy\Matcher\Matcher;
+use Pimcore\Model\DataObject\Fieldcollection;
+
+class PimcoreFieldCollectionDefinitionMatcher implements Matcher
+{
+    public function __construct(private string $matchType)
+    {
+    }
+
+    public function matches(mixed $object, mixed $property): bool
+    {
+        if ($object instanceof Fieldcollection\Data\AbstractData) {
+            $collectionDef = Fieldcollection\Definition::getByKey($object->getType());
+
+            if ($collectionDef instanceof Fieldcollection\Definition) {
+                return $collectionDef->getFieldDefinition($property) instanceof $this->matchType;
+            }
+        }
+
+        return false;
+    }
+}
